c0cdb30792782ec33bdaba7d0e4218a3bd1ab0df
daniel-pawelko-ics20 <82897918+daniel-pawelko-ics20@users.noreply.github.com>
2022-02-02 13:27:05 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,6 +10,21 @@
 using std::cout;
 using std::endl;
 
+int find_smallest(int arr[10]) {
+    // finding smallest number
+    int smallest = 101;
+
+    // find smallest/process
+    for (int num = 0; num < 10; num++) {
+        if (arr[num] < smallest) {
+            smallest = arr[num];
+        }
+    }
+
+    // return smallest
+    return smallest;
+}
+
 int main() {
     // main function for min number
 
@@ -20,24 +35,16 @@ int main() {
 
     // defining variables
     int arr[10];
-    int smallest = 101;
 
     // create array with 10 random numbers/process
     for (int rand = 0; rand < 10; rand++) {
         arr[rand] = distr(eng);
+        cout << "The random number is: " << arr[rand] << endl;
     }
 
-    // find smallest/process
-    for (int num = 0; num < 10; num++) {
-        cout << "The random number is: " << arr[num] << endl;
-        if (arr[num] < smallest) {
-            smallest = arr[num];
-        }
-    }
-
-    // output
+    // output/calling function
     cout << endl;
-    cout << "Smallest number is " << smallest << endl;
+    cout << "Smallest number is " << find_smallest(arr) << endl;
 
     // output finished
     cout << endl;
